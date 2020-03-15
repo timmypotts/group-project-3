0df005e41785ce6c006412eee12e07d193161e98
Jay M <jay@suitedconnector.com>
2020-03-14 22:24:14 -0600

    Fixes

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
-}
+const PORT = process.env.PORT || 3001;
+const db = require("./models");
+const routes = require('./routes/api-routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var db = require("./models");
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-require("./routes/api-routes.js")(app);
 // Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
+app.use(routes);
 
-require("./routes/api-routes.js")(app);
+
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
